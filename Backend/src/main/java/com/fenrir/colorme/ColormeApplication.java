b01e71e2fe3688d55f82407fb8659bec0dd391aa
Fenrir7734 <hitex1999@gmail.com>
2023-02-27 17:38:43 +0100

    Init backend

---
+++ Backend/src/main/java/com/fenrir/colorme/ColormeApplication.java
@@ -0,0 +1,13 @@
+package com.fenrir.colorme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColormeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColormeApplication.class, args);
+    }
+
+}
