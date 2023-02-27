f56565e5ed1f201bfda58b34b7fabe955f32bcf0
Fenrir7734 <hitex1999@gmail.com>
2023-02-27 18:42:16 +0100

    Added swagger

---
+++ Backend/src/main/java/com/fenrir/colorme/config/OpenApiConfiguration.java
@@ -0,0 +1,9 @@
+package com.fenrir.colorme.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Colorme Spring Rest API"))
+public class OpenApiConfiguration { }
