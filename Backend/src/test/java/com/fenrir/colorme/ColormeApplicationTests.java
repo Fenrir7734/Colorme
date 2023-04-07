bb3d444641cef6ab3d2b927a86a8d4a0d6036e0c
Fenrir7734 <hitex1999@gmail.com>
2023-04-07 19:22:53 +0200

    Fix tests

--- Backend/src/test/java/com/fenrir/colorme/ColormeApplicationTests.java
+++ Backend/src/test/java/com/fenrir/colorme/ColormeApplicationTests.java
@@ -1,9 +1,12 @@
 package com.fenrir.colorme;
 
+import com.fenrir.colorme.shared.PostgreSQLContainerInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = {PostgreSQLContainerInitializer.class})
 class ColormeApplicationTests {
 
     @Test
