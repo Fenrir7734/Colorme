33795a19b6f15a7f24bbf04a2eb70a09146d46a5
Fenrir7734 <hitex1999@gmail.com>
2023-05-14 10:21:07 +0200

    Check if user is admin

--- Backend/src/main/java/com/fenrir/colorme/common/security/AuthenticationFacade.java
+++ Backend/src/main/java/com/fenrir/colorme/common/security/AuthenticationFacade.java
@@ -1,8 +1,10 @@
 package com.fenrir.colorme.common.security;
 
 import com.fenrir.colorme.common.security.oauth2.user.UserPrincipal;
+import com.fenrir.colorme.user.domain.Role;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +31,23 @@ public class AuthenticationFacade {
     }
 
     private boolean isUserAuthenticated(Long userId) {
+        return getUserPrincipal().getId().equals(userId);
+    }
+
+    private UserPrincipal getUserPrincipal() {
         Authentication authentication = getAuthentication();
-        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-        return userPrincipal.getId().equals(userId);
+        return (UserPrincipal) authentication.getPrincipal();
+    }
+
+    public boolean isAdmin() {
+        return isAuthenticated() && hasRole(Role.ADMIN.getName());
+    }
+
+    private boolean hasRole(String role) {
+        return getUserPrincipal()
+                .getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .anyMatch(authority -> authority.equals(role));
     }
 }
