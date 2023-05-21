099c54eaea71c441f70a82cf706f14e086a6aad2
Fenrir7734 <hitex1999@gmail.com>
2023-05-21 10:29:21 +0200

    Fix tests of PalettePersistenceAdapter and add new onec for likes

--- Backend/src/test/java/com/fenrir/colorme/palette/adapter/out/persistence/PalettePersistenceAdapterTest.java
+++ Backend/src/test/java/com/fenrir/colorme/palette/adapter/out/persistence/PalettePersistenceAdapterTest.java
@@ -2,6 +2,7 @@ package com.fenrir.colorme.palette.adapter.out.persistence;
 
 import com.fenrir.colorme.palette.domain.Palette;
 import com.fenrir.colorme.palette.domain.PaletteColor;
+import com.fenrir.colorme.palette.domain.PaletteLike;
 import com.fenrir.colorme.shared.PersistenceAdapterTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Sql("PalettePersistenceAdapterTest.sql")
 class PalettePersistenceAdapterTest extends PersistenceAdapterTest {
     private static final String EXISTING_PALETTE_CODE = "1234";
+    private static final String EXISTING_PALETTE_WITHOUT_LIKES_CODE = "5678";
     private static final String NOT_EXISTING_PALETTE_CODE = "not existing";
+
+    private static final Long EXISTING_PALETTE_WITHOUT_LIKES_ID = 101L;
+
+    private static final Long PALETTE_OWNER_ID = 1L;
+    private static final Long ADMIN_ID = 3L;
 
     private static final Long EXISTING_TAG_1_ID = 100L;
     private static final Long EXISTING_TAB_2_ID = 200L;
 
-    private static final Integer EXISTING_PALETTES_COUNT = 1;
+    private static final Integer EXISTING_PALETTES_COUNT = 2;
+    private static final Integer LIKES_COUNT = 3;
 
     @Autowired
     private PalettePersistenceAdapter palettePersistenceAdapter;
@@ -45,7 +53,7 @@ class PalettePersistenceAdapterTest extends PersistenceAdapterTest {
         Optional<Palette> result = palettePersistenceAdapter.getPalette(EXISTING_PALETTE_CODE);
 
         // then
-        assertThat(result.isPresent()).isTrue();
+        assertThat(result).isPresent();
         assertThat(result.get().getCode().trim()).isEqualTo(EXISTING_PALETTE_CODE);
     }
 
@@ -55,7 +63,7 @@ class PalettePersistenceAdapterTest extends PersistenceAdapterTest {
         Optional<Palette> result = palettePersistenceAdapter.getPalette(NOT_EXISTING_PALETTE_CODE);
 
         // then
-        assertThat(result.isPresent()).isFalse();
+        assertThat(result).isNotPresent();
     }
 
     @Test
@@ -71,6 +79,24 @@ class PalettePersistenceAdapterTest extends PersistenceAdapterTest {
     void shouldReturnFalseIfPaletteNotExists() {
         // when
         boolean result = palettePersistenceAdapter.paletteExists(NOT_EXISTING_PALETTE_CODE);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldReturnTrueIfUserPaletteExists() {
+        // when
+        boolean result = palettePersistenceAdapter.paletteExists(EXISTING_PALETTE_CODE, PALETTE_OWNER_ID);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseIfUserPaletteNotExists() {
+        // when
+        boolean result = palettePersistenceAdapter.paletteExists(EXISTING_PALETTE_CODE, ADMIN_ID);
 
         // then
         assertThat(result).isFalse();
@@ -115,9 +141,48 @@ class PalettePersistenceAdapterTest extends PersistenceAdapterTest {
 
         // then
         assertThat(paletteRepository.count()).isEqualTo(EXISTING_PALETTES_COUNT - 1);
-        assertThat(paletteRepository.findByCode(EXISTING_PALETTE_CODE).isPresent()).isFalse();
+        assertThat(paletteRepository.findByCode(EXISTING_PALETTE_CODE)).isNotPresent();
         assertThat(paletteTagRepository.findAllByPaletteCode(EXISTING_PALETTE_CODE)).isEmpty();
         assertThat(paletteColorRepository.findAllByPaletteCode(EXISTING_PALETTE_CODE)).isEmpty();
         assertThat(paletteLikeRepository.findAllByPaletteCode(EXISTING_PALETTE_CODE)).isEmpty();
+    }
+
+    @Test
+    void shouldCreateLike() {
+        // given
+        final PaletteLike like = new PaletteLike(EXISTING_PALETTE_WITHOUT_LIKES_ID, ADMIN_ID);
+
+        // when
+        palettePersistenceAdapter.createLike(like);
+
+        // then
+        assertThat(paletteLikeRepository.count()).isEqualTo(LIKES_COUNT + 1);
+    }
+
+    @Test
+    void shouldDeleteLike() {
+        // when
+        palettePersistenceAdapter.deleteLike(EXISTING_PALETTE_CODE, ADMIN_ID);
+
+        // then
+        assertThat(paletteLikeRepository.count()).isEqualTo(LIKES_COUNT - 1);
+    }
+
+    @Test
+    void shouldReturnTrueIfPaletteLikeExists() {
+        // when
+        boolean result = palettePersistenceAdapter.likeExits(EXISTING_PALETTE_CODE, ADMIN_ID);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseIfPaletteLikeNotExists() {
+        // when
+        boolean result = palettePersistenceAdapter.likeExits(EXISTING_PALETTE_WITHOUT_LIKES_CODE, ADMIN_ID);
+
+        // then
+        assertThat(result).isFalse();
     }
 }
--- Backend/src/test/resources/com/fenrir/colorme/palette/adapter/out/persistence/PalettePersistenceAdapterTest.sql
+++ Backend/src/test/resources/com/fenrir/colorme/palette/adapter/out/persistence/PalettePersistenceAdapterTest.sql
@@ -1,12 +1,23 @@
+INSERT INTO users (id, code, provider, external_id, external_name, email, role, created_at, updated_at)
+VALUES (1, '1', 'GOOGLE', '1234', 'user1', 'user1@gmail.com', 'USER', current_timestamp, current_timestamp);
+INSERT INTO users (id, code, provider, external_id, external_name, email, role, created_at, updated_at)
+VALUES (2, '2', 'GOOGLE', '5678', 'user2', 'user3@gmail.com', 'USER', current_timestamp, current_timestamp);
+INSERT INTO users (id, code, provider, external_id, external_name, email, role, created_at, updated_at)
+VALUES (3, '3', 'FACEBOOK', '4321', 'user3', 'user2@gmail.com', 'ADMIN', current_timestamp, current_timestamp);
+
 INSERT INTO tag_categories (id, code, name) VALUES (100, 100, 'Colors');
 
 INSERT INTO tags (id, code, name, category_id) VALUES (100, 100, 'Red', 100);
 INSERT INTO tags (id, code, name, category_id) VALUES (101, 101, 'Green', 100);
 
-INSERT INTO palettes (id, code, created_at) VALUES (100, '1234', current_timestamp);
+INSERT INTO palettes (id, code, owner_id, created_at) VALUES (100, '1234', 1, current_timestamp);
 INSERT INTO palette_colors (id, hex, palette_id) VALUES (100, 'FFFFFF', 100);
 INSERT INTO palette_colors (id, hex, palette_id) VALUES (101, 'AAAAAA', 100);
 INSERT INTO palette_tags (palette_id, tag_id) VALUES (100, 100);
-INSERT INTO palette_likes (id, created_at, palette_id) VALUES (100, current_timestamp, 100);
-INSERT INTO palette_likes (id, created_at, palette_id) VALUES (101, current_timestamp, 100);
-INSERT INTO palette_likes (id, created_at, palette_id) VALUES (102, current_timestamp, 100);
+INSERT INTO palette_likes (palette_id, user_id, created_at) VALUES (100, 1, current_timestamp);
+INSERT INTO palette_likes (palette_id, user_id, created_at) VALUES (100, 2, current_timestamp);
+INSERT INTO palette_likes (palette_id, user_id, created_at) VALUES (100, 3, current_timestamp);
+
+INSERT INTO palettes (id, code, owner_id, created_at) VALUES (101, '5678', 1, current_timestamp);
+INSERT INTO palette_colors (id, hex, palette_id) VALUES (102, 'FFFFFF', 101);
+INSERT INTO palette_colors (id, hex, palette_id) VALUES (103, 'AAAAAA', 101);
