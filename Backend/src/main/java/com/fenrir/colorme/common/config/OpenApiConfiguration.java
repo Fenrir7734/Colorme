9df93b0db0eeae7a7f15bac2844617934728338a
Fenrir7734 <hitex1999@gmail.com>
2023-05-22 23:13:55 +0200

    Update swagger config

--- Backend/src/main/java/com/fenrir/colorme/common/config/OpenApiConfiguration.java
+++ Backend/src/main/java/com/fenrir/colorme/common/config/OpenApiConfiguration.java
@@ -1,9 +1,29 @@
 package com.fenrir.colorme.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Colorme Spring Rest API"))
-public class OpenApiConfiguration { }
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Colorme Rest API",
+                version = "0.1.0",
+                description = "",
+                contact = @Contact(
+                        name = "F3NRIR",
+                        email = "hitex1999@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                )),
+        security = {
+                @SecurityRequirement(name = "oauth2")
+        }
+)
+public class OpenApiConfiguration {
+}
