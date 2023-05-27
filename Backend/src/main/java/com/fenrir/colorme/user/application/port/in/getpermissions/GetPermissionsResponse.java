ce40102cfd9658a5bb4c7156a2a52a08bf6eaef4
Fenrir7734 <hitex1999@gmail.com>
2023-05-27 09:28:53 +0200

    Fix CORS

--- Backend/src/main/java/com/fenrir/colorme/user/application/port/in/getpermissions/GetPermissionsResponse.java
+++ Backend/src/main/java/com/fenrir/colorme/user/application/port/in/getpermissions/GetPermissionsResponse.java
@@ -1,11 +1,14 @@
 package com.fenrir.colorme.user.application.port.in.getpermissions;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.util.List;
 
 @Value
 public class GetPermissionsResponse {
+
+    @JsonProperty("userId")
     String code;
     List<String> permissions;
 }
